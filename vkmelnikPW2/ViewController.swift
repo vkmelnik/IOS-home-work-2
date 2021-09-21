d149e7f62f434dec58e4fafddb28685f42e79737
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-09-21 17:41:24 +0300

    Initial commit

---
+++ vkmelnikPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkmelnikPW2
+//
+//  Created by Vsevolod Melnik on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
