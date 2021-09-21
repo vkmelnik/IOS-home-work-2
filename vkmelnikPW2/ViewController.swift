b0ed310f6c3046b857eed1ba36e2524ff3a8da30
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-09-21 18:44:28 +0300

    Pressing button makes settings appear/disappear

--- vkmelnikPW2/ViewController.swift
+++ vkmelnikPW2/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let settingsView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSettingsView()
         setupSettingsButton()
     }
 
@@ -38,6 +40,35 @@ class ViewController: UIViewController {
             = true
         settingsButton.widthAnchor.constraint(equalTo:
             settingsButton.heightAnchor).isActive = true
+        
+        settingsButton.addTarget(self, action: #selector(settingsButtonPressed),
+                                 for: .touchUpInside)
+    }
+    
+    private func setupSettingsView() {
+        view.addSubview(settingsView)
+        settingsView.translatesAutoresizingMaskIntoConstraints = false
+        settingsView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 10
+        ).isActive = true
+        settingsView.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -10 ).isActive = true
+        settingsView.heightAnchor.constraint(equalToConstant: 300).isActive
+            = true
+        settingsView.widthAnchor.constraint(equalToConstant: 200).isActive
+            = true
+        
+        settingsView.backgroundColor = .white
+        settingsView.alpha = 0
+    }
+    
+    @objc
+    private func settingsButtonPressed() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.settingsView.alpha = 1 - self.settingsView.alpha
+        })
     }
 }
 
