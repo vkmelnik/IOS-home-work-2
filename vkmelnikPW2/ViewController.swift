231758b4b603131eda71492248877447e4a65d1b
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-09-21 18:29:48 +0300

    Settings button added

--- vkmelnikPW2/ViewController.swift
+++ vkmelnikPW2/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSettingsButton()
     }
 
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private func setupSettingsButton() {
+        let settingsButton = UIButton(type: .system)
+        view.addSubview(settingsButton)
+        settingsButton.setImage(
+            UIImage(named: "settings")?.withRenderingMode(.alwaysOriginal),
+            for: .normal
+        )
+        settingsButton.translatesAutoresizingMaskIntoConstraints = false
+        settingsButton.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 15
+        ).isActive = true
+        settingsButton.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -15 ).isActive = true
+        settingsButton.heightAnchor.constraint(equalToConstant: 30).isActive
+            = true
+        settingsButton.widthAnchor.constraint(equalTo:
+            settingsButton.heightAnchor).isActive = true
+    }
 }
 
