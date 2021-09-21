32cbbc2761781b157abe1bd8ffb6d850e3a0b230
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-09-21 19:09:25 +0300

    Location toggle added

--- vkmelnikPW2/ViewController.swift
+++ vkmelnikPW2/ViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     private let settingsView = UIView()
+    private let locationTextView = UITextView()
+    private let locationManager = CLLocationManager()
+    private let locationToggle = UISwitch()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupLocationTextView()
         setupSettingsView()
         setupSettingsButton()
+        setupLocationToggle()
+        setupLocationManager()
+        
+        locationManager.requestWhenInUseAuthorization()
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -45,6 +55,25 @@ class ViewController: UIViewController {
                                  for: .touchUpInside)
     }
     
+    private func setupLocationManager() {
+        let locationLabel = UILabel()
+        settingsView.addSubview(locationLabel)
+        locationLabel.text = "Location"
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        locationLabel.topAnchor.constraint(
+            equalTo: settingsView.topAnchor,
+            constant: 55
+        ).isActive = true
+        locationLabel.leadingAnchor.constraint(
+            equalTo: settingsView.leadingAnchor,
+            constant: 10
+        ).isActive = true
+        locationLabel.trailingAnchor.constraint(
+            equalTo: locationToggle.leadingAnchor,
+            constant: -10
+        ).isActive = true
+    }
+    
     private func setupSettingsView() {
         view.addSubview(settingsView)
         settingsView.translatesAutoresizingMaskIntoConstraints = false
@@ -63,6 +92,45 @@ class ViewController: UIViewController {
         settingsView.backgroundColor = .white
         settingsView.alpha = 0
     }
+
+    private func setupLocationTextView() {
+        view.addSubview(locationTextView)
+        
+        locationTextView.backgroundColor = .white
+        locationTextView.layer.cornerRadius = 20
+        locationTextView.translatesAutoresizingMaskIntoConstraints = false
+        locationTextView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 60
+        ).isActive = true
+        locationTextView.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        locationTextView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        locationTextView.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor,
+            constant: 15
+        ).isActive = true
+        locationTextView.isUserInteractionEnabled = false
+    }
+    
+    private func setupLocationToggle() {
+        settingsView.addSubview(locationToggle)
+        locationToggle.translatesAutoresizingMaskIntoConstraints = false
+        locationToggle.topAnchor.constraint(
+            equalTo: settingsView.topAnchor,
+            constant: 50
+        ).isActive = true
+        locationToggle.trailingAnchor.constraint(
+            equalTo: settingsView.trailingAnchor,
+            constant: -10
+        ).isActive = true
+        locationToggle.addTarget(
+            self,
+            action: #selector(locationToggleSwitched),
+            for: .valueChanged
+        )
+    }
     
     @objc
     private func settingsButtonPressed() {
@@ -70,5 +138,30 @@ class ViewController: UIViewController {
             self.settingsView.alpha = 1 - self.settingsView.alpha
         })
     }
+    
+    @objc
+    func locationToggleSwitched(_ sender: UISwitch) {
+        if sender.isOn {
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.delegate = self
+                locationManager.desiredAccuracy =
+                kCLLocationAccuracyNearestTenMeters
+                locationManager.startUpdatingLocation()
+            } else {
+                sender.setOn(false, animated: true)
+            }
+        } else {
+            locationTextView.text = ""
+            locationManager.stopUpdatingLocation()
+        }
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        guard let coord: CLLocationCoordinate2D =
+                manager.location?.coordinate else { return }
+        locationTextView.text = "Coordinates = \(coord.latitude) \(coord.longitude)"
+    }
+}
